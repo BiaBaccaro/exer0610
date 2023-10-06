769bb5b244482210c95c5b248e5ba1845d656a2c
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-10-06 15:35:23 -0300

    commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        include "teste.php";
+    ?>
+
+    <p>Após o include</p>
+    <p>O conteúdo do programa teste.php é: <?php echo $c; ?></p>
+
+
+
+</body>
+</html>
---
+++ teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $c = 5;
+    ?>
+        <h1>Arquivo teste.php com a declaração da variável c</h1>
+
+</body>
+</html>
